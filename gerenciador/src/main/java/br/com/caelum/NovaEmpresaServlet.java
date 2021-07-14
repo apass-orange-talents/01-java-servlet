691e1b59865bc73cee8148f621e9b174de2d6264
Anderson Passos Silva <anderson.silva@zup.com.br>
2021-07-14 12:10:05 -0300

    Adiciona projeto gerenciador com Servlet

---
+++ gerenciador/src/main/java/br/com/caelum/NovaEmpresaServlet.java
@@ -0,0 +1,22 @@
+package br.com.caelum;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/novaEmpresa")
+public class NovaEmpresaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		
+		out.print("Empresa " + req.getParameter("nome") + " cadastradas com sucesso!");
+	}
+}
---
+++ gerenciador/src/main/java/br/com/caelum/OiMundo.java
@@ -0,0 +1,30 @@
+package br.com.caelum;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/oi")
+public class OiMundo extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<meta charset=\"utf-8\">");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("Olá Mundo!");
+		out.print("</body>");
+		out.print("</html>");
+	}
+
+}
